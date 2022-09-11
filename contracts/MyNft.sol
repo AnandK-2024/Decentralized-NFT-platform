c49bfee412472d013e33f0272877a55d074c65c0
AnandK-2024 <anandkumar@iitbhilai.ac.in>
2022-09-11 21:26:43 +0530

    pragma correction

--- contracts/MyNft.sol
+++ contracts/MyNft.sol
@@ -1,5 +1,5 @@
 //SPDX-License-Identifier: Unlicense
-pragma solidity 0.8.9;
+pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
 import "@openzeppelin/contracts/utils/Counters.sol";
--- contracts/NftMarket.sol
+++ contracts/NftMarket.sol
@@ -1,5 +1,5 @@
 //SPDX-License-Identifier: Unlicense
-pragma solidity 0.8.9;
+pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
