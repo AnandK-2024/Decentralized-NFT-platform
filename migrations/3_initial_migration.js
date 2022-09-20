e6e9a8d51cfcb053b95cd715a57355dc196b8c37
AnandK-2024 <anandkumar@iitbhilai.ac.in>
2022-09-20 20:03:30 +0530

    problem in 3_initial_migration.js for deploy contract

--- migrations/3_initial_migration.js
+++ migrations/3_initial_migration.js
@@ -1,13 +1,22 @@
 // const { artifacts } = require("hardhat");
 
-const MyNft = artifacts.require("MyNft");
+// const { artifacts } = require("hardhat");
+// const Migrations = artifacts.require("Migrations");
+
+const NftMarket = artifacts.require("NftMarket");
+const MyNft = artifacts.require('MyNft')
 // error in excess address from 2_initial_migration.js
-const address= artifacts.require("./2_initial_migration");
-console.log("imported address",address)
-module.exports = function (deployer) {
+// console.log("imported address",address)
+module.exports = async function (deployer) {
+    var address;
+    await deployer.deploy(NftMarket).then(() => {
+
+    address = NftMarket.address
+    console.log("this is the address of the NFTMarkewt",address)
+    })
+    console.log("contract 2 is deployed. Now starting 3 with address", address)
     deployer.deploy(MyNft("iitbhilaitoken","IITBH",address)).then(() => {
 
-    var address=NftMarket.address
     console.log("real address",NftMarket.address,address)
 
     }
